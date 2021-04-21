1736d88b63bf657f980396148b73511cb50bb3da
Mr-luo-hm <l2509991676@163.com>
2021-04-22 01:53:35 +0800

    add es searchAfter and scorell

--- springBoot-shiro/SSM-Model/src/main/java/cn/ntshare/laboratory/serivice/EsService.java
+++ springBoot-shiro/SSM-Model/src/main/java/cn/ntshare/laboratory/serivice/EsService.java
@@ -1,7 +1,20 @@
 package cn.ntshare.laboratory.serivice;
 
 import java.io.IOException;
+import java.util.Map;
 
 public interface EsService {
     Boolean createIndex(String index,String type) throws IOException;
+
+    /**
+     * 游标查询
+     */
+    void getFriendRecordByEsScroll(String INDEX);
+
+    /**
+     * SearchAfter 查询拿10条会导致数据丢失 未详细测试
+     * @param INDEX
+     * @throws IOException
+     */
+    void getFriendRecordByEsSearchAfter(String INDEX) throws IOException;
 }
--- springBoot-shiro/SSM-Model/src/main/java/cn/ntshare/laboratory/serivice/impl/EsServiceImpl.java
+++ springBoot-shiro/SSM-Model/src/main/java/cn/ntshare/laboratory/serivice/impl/EsServiceImpl.java
@@ -2,19 +2,36 @@ package cn.ntshare.laboratory.serivice.impl;
 
 import cn.ntshare.laboratory.serivice.EsService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.search.*;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.Operator;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.RangeQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.sort.FieldSortBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.io.IOException;
+import java.time.ZoneOffset;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EsServiceImpl implements EsService {
     private final RestHighLevelClient restHighLevelClient;
 
@@ -34,5 +51,127 @@ public class EsServiceImpl implements EsService {
         CreateIndexRequest mapping = request.mapping(type, builder);
         CreateIndexResponse response = restHighLevelClient.indices().create(request, RequestOptions.DEFAULT);
         return null;
+    }
+
+    public void getFriendRecordByEsScroll(String INDEX) {
+// 1、创建search请求
+        SearchRequest searchRequest = new SearchRequest(INDEX);
+        // searchRequest.types(EsConstant.TYPE);
+        // 2、用SearchSourceBuilder来构造查询请求体 ,请仔细查看它的方法，构造各种查询的方法都在这。
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+
+        // 集合查询
+        BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
+
+        RangeQueryBuilder sendTimeQueryBuilder = QueryBuilders.rangeQuery("send_time");
+
+        sourceBuilder.query(queryBuilder);
+        // 设置一个可选的超时时间，以控制允许搜索的时间。
+        sourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS));
+        // 指定排序
+        sourceBuilder.sort(new FieldSortBuilder("send_time").order(SortOrder.DESC));
+        // 每次取1000条
+        sourceBuilder.size(1000);
+        // 将请求体加入到请求中
+        searchRequest.source(sourceBuilder);
+        // 设置滚屏
+        searchRequest.scroll(new TimeValue(20000));
+        SearchResponse searchResponse = null;
+        try {
+            searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 获取第一次得游标
+        String scrollId = searchResponse.getScrollId();
+        // 处理命中文档
+        SearchHit[] searchHits = searchResponse.getHits().getHits();
+        List<Map> mapList = new ArrayList<>();
+        for (SearchHit hit : searchHits) {
+            // 取_source字段值
+            mapList.add(hit.getSourceAsMap());
+        }
+        while (searchHits != null && searchHits.length > 0) {
+            SearchScrollRequest scrollRequest = new SearchScrollRequest(scrollId);
+            scrollRequest.scroll(new TimeValue(20000));
+            try {
+                // 带着上次游标查询
+                searchResponse = restHighLevelClient.scroll(scrollRequest, RequestOptions.DEFAULT);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // 重新给游标赋值
+            scrollId = searchResponse.getScrollId();
+            // 处理命中数据
+            searchHits = searchResponse.getHits().getHits();
+            if (searchHits != null && searchHits.length > 0) {
+                for (SearchHit searchHit : searchHits) {
+                    mapList.add(searchHit.getSourceAsMap());
+                }
+            }
+        }
+        // 清除游标
+        ClearScrollRequest clearScrollRequest = new ClearScrollRequest();
+        // 最后一次得游标
+        clearScrollRequest.addScrollId(scrollId);
+        ClearScrollResponse clearScrollResponse = null;
+        try {
+            // 清除游标 释放内存
+            clearScrollResponse = restHighLevelClient.clearScroll(clearScrollRequest, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            log.error("clear scrollId error", e);
+        }
+        boolean succeeded = false;
+        if (clearScrollResponse != null) {
+            succeeded = clearScrollResponse.isSucceeded();
+        }
+        log.info("clear scrollId status:{}", succeeded);
+        Map<String, Object> result = new HashMap(8);
+        /*result.put("list", fomartClientMsg(mapList));
+        return result;*/
+    }
+
+    @Override
+    public void getFriendRecordByEsSearchAfter(String INDEX) throws IOException {
+        // 1、创建search请求
+        SearchRequest searchRequest = new SearchRequest(INDEX);
+        // 2、用SearchSourceBuilder来构造查询请求体 ,请仔细查看它的方法，构造各种查询的方法都在这。
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+
+        // 集合查询
+        BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
+
+        RangeQueryBuilder send_timeQueryBuilder = QueryBuilders.rangeQuery("send_time");
+        sourceBuilder.query(queryBuilder);
+        // 设置一个可选的超时时间，以控制允许搜索的时间。
+        sourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS));
+        // 指定排序
+        sourceBuilder.sort(new FieldSortBuilder("send_time").order(SortOrder.DESC));
+        // 每次获取数据条数
+        sourceBuilder.size(50);
+        boolean type = true;
+        Object[] objects = null;
+        SearchResponse searchResponse;
+        Map<String, Object> result = new HashMap(8);
+        List<Map> mapList = new ArrayList<>();
+        while (type) {
+            if (objects != null) {
+                sourceBuilder.searchAfter(objects);
+            }
+            searchRequest.source(sourceBuilder);
+            searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+            SearchHit[] hits = searchResponse.getHits().getHits();
+            if (hits.length == 0) {
+                type = false;
+            }
+            if (hits.length > 0) {
+                objects = hits[hits.length - 1].getSortValues();
+            }
+            for (SearchHit searchHit : hits) {
+                mapList.add(searchHit.getSourceAsMap());
+            }
+            //result.put("list", fomartClientMsg(mapList));
+        }
+        //return result;
     }
 }
